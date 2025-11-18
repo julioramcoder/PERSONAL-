34dc67b00450cc7400667f5828e6ff8b06baf799
julioramcoder <julioramirez79@gmail.com>
2025-11-17 19:22:47 -0500

    primer commit

---
+++ typesoffunctions/FunConArgAndConRetur.py
@@ -0,0 +1,8 @@
+# el ejemplo mas sencillos de argumentos y de returnos son las operaciones matematicas
+def suma (a,b):
+    suma= a + b
+    return suma
+a = int (input("numero a"))
+b = int (input("numero a"))
+print (suma (a,b)) # siempre se va a pedir que lo que se retorne es la funcion y si quieres ver los resultados, pides que se muestren la funcion 
+# y sera la funcion que te traiga lo que ella hace 
---
+++ typesoffunctions/FunSinArgAndSinReturn.py
@@ -0,0 +1,10 @@
+#solo se usa cuando quiero mostrar un mensaje o cualquier tipo de informacion 
+
+def mostrar_menu ():
+    print("suma")
+    print("resta")
+    print("division")
+mostrar_menu() #aqui estamos llamando la funcion que se despliegue
+#no usamos mas el print, porque la misma funcion ya definio que se deben mostrar. 
+
+
